468285ce763367295e48c6aff21b910d635e316f
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2024-05-14 08:07:41 +0530

    Add files via upload

---
+++ Lecture-48/index.js
@@ -0,0 +1,45 @@
+let src = {
+    age:12,
+    wt:68,
+    ht:180
+};
+
+// console.log(obj);
+// obj.color = "white";
+// console.log(obj);
+
+
+// let  des = {...src};
+
+// src.age = 90;
+
+
+// console.log("src: ",src);
+// console.log("des: ",des);
+
+// let src2 = {
+//     value:101,
+//     name:"Anjali Singh"
+// };
+
+// let dest = Object.assign({},src,src2);
+// console.log("src: ",src);
+// console.log("dest: ",dest);
+
+// let dest = {};
+// // closing using iteration
+// for(let key in src) {
+//      let newKey = key;
+//      let newValue = src[key];
+
+//      // insert new key and value in destoination and create a clone
+//      dest[newKey] = newValue;
+// }
+
+// src.age = 90;
+
+// console.log("src: ",src);
+// console.log("dest: ",dest);
+
+
+
