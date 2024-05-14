33af0a8d2927ff299a6bc36c8adee4320380e18a
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2024-05-14 08:07:56 +0530

    Add files via upload

---
+++ Lecture-49/index.js
@@ -0,0 +1,34 @@
+// console.log(Math.PI);
+
+// console.log(Math.max(23,78,99));
+// console.log(Math.min(34,29,10));
+
+// console.log(Math.round(1.6));
+// console.log(Math.floor(1.4));    // just smallest number
+// console.log(Math.ceil(1.7));     // just greatest number
+
+// console.log(Math.abs(-6.8));
+// console.log(Math.random());
+
+// console.log(Math.sqrt(169));
+// console.log(Math.pow(2,5));
+
+// console.log(Math.sin(90));
+// console.log(Math.cos(0));
+// console.log(Math.tan(0));
+
+let curr = new Date();
+
+let date =  new Date('September 5 2003 13:30');
+
+// console.log(curr);
+console.log(date);
+
+let newDate = new Date(2003,8,5,1);
+console.log(newDate);
+
+console.log(newDate.getDay());
+
+console.log(newDate.getFullYear());
+console.log(newDate.setFullYear(2001));
+console.log(newDate);
