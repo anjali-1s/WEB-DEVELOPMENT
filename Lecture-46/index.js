0eaa0131e55f798b87fcb598b048183dc063758d
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2024-05-14 08:07:09 +0530

    Add files via upload

---
+++ Lecture-46/index.js
@@ -0,0 +1,46 @@
+// global scope
+// var age = 15;
+
+// console.log(age);
+
+// {
+//     console.log(age);
+// }
+
+// if(true){
+//     console.log(age);
+// }
+
+// function sayhello(){
+//     console.log(age);
+// }
+
+// for(let i =0; i <2; i++){
+//     console.log(age);
+// }
+
+// function scope
+// function SayHello(){
+//     var name = "earth";
+//     console.log("hello duniya",name);
+// }
+
+// SayHello();
+// console.log(name);
+
+
+// block space
+// {
+//     let num = 20;
+//     console.log(num);
+// }
+
+
+// temporal dead zone
+console.log(age);
+console.log("love");
+console.log("babbar");
+
+// variable declaration 
+let age = 34;
+console.log(age);
