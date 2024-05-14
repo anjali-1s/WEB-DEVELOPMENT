e2a1b1c784c5edbb30fa4055abb7ec78149b4432
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2024-05-14 08:08:44 +0530

    Add files via upload

---
+++ Lecture-52/index.js
@@ -0,0 +1,36 @@
+// let paraelement = document.getElementById('spara');
+// console.log(paraelement.innerText);
+
+// paraelement.style.backgroundColor = "blue";
+
+// let secondElement = document.getElementById('sdiv');
+// console.log(secondElement.style.cssText);
+// secondElement.style.cssText = "background-color:black; padding:1.5rem; color:white";
+
+// let FirstElement = document.querySelector('#fdiv');
+// FirstElement.setAttribute('class','fclass');
+// console.log(FirstElement);
+
+// FirstElement.setAttribute('style',"padding:5rem; background-color:olive;");
+
+
+// let fpara = $0;
+// console.log(fpara);
+// fpara.className = "Gohan  Bulbul";
+
+
+let Fpara = document.querySelector('#fpara');
+// console.log(Fpara.classList);
+
+// Fpara.classList.add('thirdClass');
+// console.log(Fpara.classList);
+// Fpara.classList.remove('firstclass');
+// console.log(Fpara.classList);
+Fpara.classList.toggle('secondclass');
+console.log(Fpara.classList);
+
+Fpara.classList.toggle('secondclass');
+console.log(Fpara.classList);
+
+console.log(Fpara.classList.contains('thirdclass'));
+console.log(Fpara.classList.contains('secondclass'));
